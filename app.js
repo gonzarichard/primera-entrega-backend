edd20bd0b61f62792481fcbefa4dd9f96d19d911
gonzalo <gonzalorichard30@gmail.com>
2026-03-11 17:36:28 -0300

    terminado segunda entrega

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
+import express from "express";
+import { engine } from "express-handlebars";
+import { Server } from "socket.io";
+import http from "http";
 
 const app = express();
 
